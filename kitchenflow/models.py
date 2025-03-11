4771434eb5f176e6827d25e2b71bf6ec964c7de1
yura <pollo7964132@gmail.com>
2025-03-11 17:11:12 +0200

    Create Cook Model

--- kitchenflow/models.py
+++ kitchenflow/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.PositiveIntegerField()
+
+    class Meta:
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+
+    def __str__(self) -> str:
+        return (f""
+                f"{self.first_name} {self.last_name} "
+                f"({self.years_of_experience} years_of_experience)"
+            )
