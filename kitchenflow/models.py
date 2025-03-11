0972c4284f971d39f9cf48347546fad2594d00a8
yura <pollo7964132@gmail.com>
2025-03-11 17:12:18 +0200

    Create Dish Model

--- kitchenflow/models.py
+++ kitchenflow/models.py
@@ -26,3 +26,17 @@ class DishType(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(Cook, related_name="dishes")
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return f"{self.name} - {self.price}$"
