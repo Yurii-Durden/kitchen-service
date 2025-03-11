3894b393dd9aea8c90f78855715ff680d32d9236
yura <pollo7964132@gmail.com>
2025-03-11 16:45:57 +0200

    add kitchenflow to INSTALLED_APP and applied black to app and project directories

--- kitchenflow/apps.py
+++ kitchenflow/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class KitchenflowConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'kitchenflow'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "kitchenflow"
