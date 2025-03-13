24f03c3a959cbfa868945521be4659c37a95bc67
yura <pollo7964132@gmail.com>
2025-03-13 19:31:53 +0200

    create index func for home page

--- kitchenflow/views.py
+++ kitchenflow/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from kitchenflow.models import Cook, Dish, DishType
+
+def index(request) -> HttpResponse:
+    context = {
+        "count_of_cooks": Cook.objects.count(),
+        "count_of_dishes": Dish.objects.count(),
+        "count_of_dishes_type": DishType.objects.count(),
+    }
+
+    return render(request,"kitchenflow/home_page.html", context=context)
