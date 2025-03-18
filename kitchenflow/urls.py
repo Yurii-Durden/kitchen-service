d7f74114e15fff69ad8090bb2334670d9ff3568c
yura <pollo7964132@gmail.com>
2025-03-18 19:52:59 +0200

    add detail paths

--- kitchenflow/urls.py
+++ kitchenflow/urls.py
@@ -9,6 +9,9 @@ app_name = "kitchenflow"
 urlpatterns = [
     path("", views.index, name="index"),
     path("cooks/", views.CookListView.as_view(), name="cook-list"),
+    path("cooks/<int:pk>", views.CookDetailView.as_view(), name="cook-detail"),
     path("dishes/", views.DishListView.as_view(), name="dish-list"),
+    path("dishes/<int:pk>", views.DishDetailView.as_view(), name="dish-detail"),
     path("dish-types/", views.DishTypeListView.as_view(), name="dish-type-list"),
+    path("dish-types/<int:pk>", views.DishTypeDetailView.as_view(), name="dish-type-detail")
 ]
