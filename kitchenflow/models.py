58e41764a90ac5db35d64fd07a104aef0bae77a1
yura <pollo7964132@gmail.com>
2025-03-19 19:29:19 +0200

    add related_name='dishes' for dish_type field in Dish model

--- kitchenflow/models.py
+++ kitchenflow/models.py
@@ -32,7 +32,7 @@ class Dish(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE, related_name="dishes")
     cooks = models.ManyToManyField(Cook, related_name="dishes")
 
     class Meta:
