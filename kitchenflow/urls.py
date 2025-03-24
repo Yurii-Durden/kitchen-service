3122ec29137e05a2bc5ad5289b32d8258ce22710
yura <pollo7964132@gmail.com>
2025-03-24 17:55:59 +0200

    add path for cook creation

--- kitchenflow/urls.py
+++ kitchenflow/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("cooks/", views.CookListView.as_view(), name="cook-list"),
     path("cooks/<int:pk>", views.CookDetailView.as_view(), name="cook-detail"),
+    path("cooks/create/", views.CookCreateView.as_view(), name="cook-create"),
     path("dishes/", views.DishListView.as_view(), name="dish-list"),
     path("dishes/<int:pk>", views.DishDetailView.as_view(), name="dish-detail"),
     path("dish-types/", views.DishTypeListView.as_view(), name="dish-type-list"),
