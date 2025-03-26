31e0ee106ae146dc45335dc1a9cd839c30b5dba4
yura <pollo7964132@gmail.com>
2025-03-26 22:37:32 +0200

    add min value validator for price in Dish Model

--- kitchenflow/models.py
+++ kitchenflow/models.py
@@ -39,8 +39,11 @@ class DishType(models.Model):
 
 class Dish(models.Model):
     name = models.CharField(max_length=255, unique=True)
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2,
+        validators=[MinValueValidator(1)]
+    )
     description = models.TextField(blank=True, null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
     dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE, related_name="dishes")
     cooks = models.ManyToManyField(Cook, related_name="dishes")
 
