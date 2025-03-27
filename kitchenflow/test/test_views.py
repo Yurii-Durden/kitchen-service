1e17d08442287f61e35ab8ff51ce8dcd2b9b72a0
yura <pollo7964132@gmail.com>
2025-03-27 22:34:10 +0200

    add search tests for all models

--- kitchenflow/test/test_views.py
+++ kitchenflow/test/test_views.py
@@ -38,3 +38,75 @@ class PrivateDishTypeListViewTest(TestCase):
             list(dish_types)
         )
         self.assertTemplateUsed(response, "kitchenflow/type_of_dishes_list.html")
+
+
+class SearchTest(TestCase):
+    def setUp(self):
+        user = get_user_model().objects.create(
+            username="test_name",
+            password="test_password12345"
+        )
+        self.client.force_login(user)
+
+    def test_search_for_dish_type_list(self):
+        DishType.objects.create(name="name1")
+        DishType.objects.create(name="name2")
+        DishType.objects.create(name="name3")
+        query_params = {"name": "name1"}
+
+        full_url = f"{DISH_TYPE_LIST_URL}?{urlencode(query_params)}"
+        response = self.client.get(full_url)
+        self.assertEquals(
+            list(DishType.objects.filter(name__icontains="name1")),
+            list(response.context["dish_type_list"])
+        )
+
+    def test_search_for_cook_list(self):
+        get_user_model().objects.create(
+            username="user1",
+            password="password12345",
+            years_of_experience=0
+        )
+        get_user_model().objects.create(
+            username="user2",
+            password="password12345",
+            years_of_experience=1
+        )
+        get_user_model().objects.create(
+            username="user3",
+            password="password12345",
+            years_of_experience=10
+        )
+        full_url = (
+            f""
+            f"{reverse('kitchenflow:cook-list')}"
+            f"?{urlencode({'username':'u'})}"
+        )
+        response = self.client.get(full_url)
+        self.assertEquals(
+            list(get_user_model().objects.filter(username__icontains="u")[:2]),
+            list(response.context["cooks_list"])
+        )
+
+    def test_search_for_dish_list(self):
+        dish_type = DishType.objects.create(name="test_type_name")
+        cook = get_user_model().objects.create(username="test_name_user")
+        Dish.objects.create(name="test1", price=10, dish_type=dish_type)
+        Dish.objects.create(name="dish2", price=10, dish_type=dish_type)
+        Dish.objects.create(name="test3", price=10, dish_type=dish_type)
+
+        full_url = f"{reverse('kitchenflow:dish-list')}?{urlencode({'name':'test'})}"
+        response = self.client.get(full_url)
+
+        self.assertEquals(
+            list(response.context["dish_list"]),
+            list(Dish.objects.filter(name__icontains="test"))
+        )
+
+        url_without_filters = reverse("kitchenflow:dish-list")
+        response_without_filters = self.client.get(url_without_filters)
+
+        self.assertEquals(
+            list(response_without_filters.context["dish_list"]),
+            list(Dish.objects.all()[:2])
+        )
