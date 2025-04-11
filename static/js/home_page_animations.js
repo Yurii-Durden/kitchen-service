a21de755643410235587bfc47a721b8b0413eda1
yura <pollo7964132@gmail.com>
2025-04-11 19:12:47 +0300

    create line section annimation with js

--- static/js/home_page_animations.js
+++ static/js/home_page_animations.js
@@ -1,44 +1,40 @@
-//
-// const svgTextMore = document.querySelector('.svg-text-more');
-// const initialX = parseFloat(svgTextMore.getAttribute('x'));
-// const targetX = initialX - 20;
-// const duration = 300; // Тривалість анімації в мілісекундах
-// let startTime;
-//
-// function animate(timestamp) {
-//   if (!startTime) startTime = timestamp;
-//   const progress = timestamp - startTime;
-//   const percentage = Math.min(1, progress / duration);
-//   const currentX = initialX + (targetX - initialX) * percentage;
-//   svgTextMore.setAttribute('x', currentX);
-//
-//   if (progress < duration) {
-//     requestAnimationFrame(animate);
-//   }
-// }
-//
-// function reverseAnimate(timestamp) {
-//   if (!startTime) startTime = timestamp;
-//   const progress = timestamp - startTime;
-//   const percentage = Math.min(1, progress / duration);
-//   const currentX = targetX + (initialX - targetX) * percentage;
-//   svgTextMore.setAttribute('x', currentX);
-//
-//   if (progress < duration) {
-//     requestAnimationFrame(reverseAnimate);
-//   }
-// }
-//
-//   svgTextMore.addEventListener('mouseover', () => {
-//     startTime = null;
-//     requestAnimationFrame(animate);
-//   });
-//
-//   svgTextMore.addEventListener('mouseout', () => {
-//     startTime = null;
-//     requestAnimationFrame(reverseAnimate);
-//   });
+const first_section_line = document.querySelector(".first_section_line");
+const second_section_line = document.querySelector(".second_section_line");
 
-// line_section animation
-const line_section_f = document.querySelector(".next-design-inspiration-box-limit")
-const line_section_s = document.querySelector(".next-project-description-box-limit")
+const second_section = document.querySelector(".second_section")
+const third_section = document.querySelector(".third_section")
+
+window.addEventListener("scroll", linesScrollAnim)
+function linesScrollAnim() {
+    const s_section_height = second_section.offsetHeight;
+    const f_section_offset = offset(second_section).top;
+    const anim_start = 2;
+
+    let s_anim_item_point = window.innerHeight - s_section_height / anim_start;
+
+    if (scrollY > f_section_offset - s_anim_item_point) {
+        first_section_line.classList.add("_active");
+    } else {
+        first_section_line.classList.remove("_active");
+    }
+
+    const t_section_height = third_section.offsetHeight;
+    const t_section_offset = offset(third_section).top;
+
+    let t_anim_item_point = window.innerHeight - t_section_height / anim_start;
+
+    if (scrollY > t_section_offset - t_anim_item_point) {
+        second_section_line.classList.add("_active");
+    } else {
+        second_section_line.classList.remove("_active");
+    }
+
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.scrollX || document.documentElement.scrollLeft,
+            scrollTop = window.scrollY || document.documentElement.scrollTop;
+        return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
+    }
+}
+
+// додавати значення скролу до відступу між великим текстом, доти, доки воно не буде рівне початковому
