871d0d86853a690f47dfaf6cad1ed889867eb8be
yura <pollo7964132@gmail.com>
2025-04-12 03:18:43 +0300

    create animation for footer

---
+++ static/js/footer_anime.js
@@ -0,0 +1,19 @@
+const footer = document.querySelector(".footer");
+const footer_line = document.querySelector(".footer-line");
+
+addEventListener("scroll", footerSrollAnimation)
+
+function footerSrollAnimation () {
+
+    if (isInViewport(footer)) {
+        footer_line.classList.add("footer-line_animate")
+    }
+
+    function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+
+    return rect.top < windowHeight - (footer.offsetHeight / 2);
+    }
+}
+
