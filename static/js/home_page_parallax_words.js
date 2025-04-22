13a88e5e9433ff0a790691c961323b2448fca1e1
yura <pollo7964132@gmail.com>
2025-04-23 00:10:41 +0300

    use gsap for paralax and words anim, create only one file for home page anims

--- static/js/home_page_parallax_words.js
+++ static/js/home_page_parallax_words.js
@@ -13,6 +13,7 @@ ScrollTrigger.create({
   onUpdate: self => {
     const progress = self.progress;
     updateImagePosition(img_vision, progress, 150, 220);
+    updateTextWidth(progress, 4, 45);
   }
 });
 
@@ -42,4 +43,11 @@ function updateImagePosition(img, progress, startPos = 150, moveUpSpeed = 220) {
   img.style.objectPosition = `center ${newPos}%`;
 }
 
+function updateTextWidth(progress, speedFactor = 0.7, maxWidth = 45) {
+  const words = document.querySelectorAll(".ani-t");
+  const newWidth = Math.min(maxWidth, speedFactor * progress * maxWidth);
 
+  words.forEach(word => {
+    word.style.width = `${newWidth}px`;
+  });
+}
