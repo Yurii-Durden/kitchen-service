f339a12911cefe519cd2c1ab5e3eb47d9536020f
yura <pollo7964132@gmail.com>
2025-05-02 20:48:49 +0300

    create media for header

--- static/js/main.js
+++ static/js/main.js
@@ -43,32 +43,32 @@ ScrollSmoother.create({
 // }
 //
 //
-// const header_menu_button = document.querySelector(".menu__item");
-// const header_menu = document.querySelector(".header__menu");
-// const menu_click_elem = document.querySelector(".menu__click__elem");
-// const body = document.body;
-//
-// header_menu_button.addEventListener("click", (event) => {
-//   event.stopPropagation();
-//   header_menu.classList.toggle("header__menu__active");
-//   menu_click_elem.classList.toggle("menu__button__active");
-//   body.classList.toggle("hide__scroll");
-// });
-//
-// let lastScroll = 0;
-// const header = document.querySelector('.header');
-// const firstSection = document.querySelector('.first__section');
-//
-// window.addEventListener('scroll', () => {
-//   const scrollTop = window.scrollY;
-//   const firstSectionTop = firstSection.getBoundingClientRect().top;
-//
-//   if (firstSectionTop <= 0 && firstSectionTop >= -50) {
-//     header.style.transform = 'translateY(0)';
-//   } else if (scrollTop > lastScroll + 5) {
-//     header.style.transform = 'translateY(-100%)';
-//   } else if (scrollTop < lastScroll - 5) {
-//     header.style.transform = 'translateY(0)';
-//   }
-//   lastScroll = scrollTop;
-// });
+const header_menu_button = document.querySelector(".menu__item");
+const header_menu = document.querySelector(".header__menu");
+const menu_click_elem = document.querySelector(".menu__click__elem");
+const body = document.body;
+
+header_menu_button.addEventListener("click", (event) => {
+  event.stopPropagation();
+  header_menu.classList.toggle("header__menu__active");
+  menu_click_elem.classList.toggle("menu__button__active");
+  body.classList.toggle("hide__scroll");
+});
+
+let lastScroll = 0;
+const header = document.querySelector('.header');
+const firstSection = document.querySelector('.first__section');
+
+window.addEventListener('scroll', () => {
+  const scrollTop = window.scrollY;
+  const firstSectionTop = firstSection.getBoundingClientRect().top;
+
+  if (firstSectionTop <= 0 && firstSectionTop >= -50) {
+    header.style.transform = 'translateY(0)';
+  } else if (scrollTop > lastScroll + 5) {
+    header.style.transform = 'translateY(-100%)';
+  } else if (scrollTop < lastScroll - 5) {
+    header.style.transform = 'translateY(0)';
+  }
+  lastScroll = scrollTop;
+});
