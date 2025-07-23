db6d15a532460c707fc5a7ca40639753083f973b
yura <pollo7964132@gmail.com>
2025-07-23 14:12:58 +0300

    change SECRETE_KEY position and update requirements.txt file

--- kitchen_service/settings.py
+++ kitchen_service/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
+
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ddjz^qtj_t-%q-+$#!mz9$n-1%gpxijn)p1%n2bqu&1#6-$hg1"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+
+if not SECRET_KEY:
+    raise ValueError("No DJANGO_SECRET_KEY set for Django settings")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
