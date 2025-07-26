dd82a424ed872f44a3bfd1833062be5230b2f978
yura <pollo7964132@gmail.com>
2025-07-26 19:43:31 +0300

    add allowed host

--- kitchen_service/settings.py
+++ kitchen_service/settings.py
@@ -32,7 +32,7 @@ if not SECRET_KEY:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
